2709c8eea3c9fd1713a393b93090471cc388bf77
Arjun Ranade <epicarjun27@gmail.com>
2024-02-26 09:35:42 -0800

    faster implementation of check_biome

--- biome.py
+++ biome.py
@@ -5,44 +5,49 @@ to the current biome/environment.
 
 import sys
 from gdpc import Editor
-from gdpc.exceptions import InterfaceConnectionError, BuildAreaNotSetError  
+from gdpc.exceptions import InterfaceConnectionError, BuildAreaNotSetError
 from gdpc.vector_tools import addY
 from collections import Counter
 import operator
 
+
+# Returns the most common biome within the set build area
 def check_biome():
     editor = Editor()
-    
+
     try:
         buildArea = editor.getBuildArea()
     except BuildAreaNotSetError:
         print("Failed to get build area")
         sys.exit(1)
-    
 
     buildRect = buildArea.toRect()
     worldSlice = editor.loadWorldSlice(buildRect)
 
+    biome_counter = Counter()
 
-    biome_counter = {}
-
-    print(buildArea)
-
-
+    
     for x in range(buildArea.begin[0], buildArea.end[0]):
         for z in range(buildArea.begin[1], buildArea.end[1]):
             for y in range(buildArea.begin[2], buildArea.end[2]):
-                biome = worldSlice.getBiome(addY((x,0,z), y))
-                if biome != '':
-                    if biome not in biome_counter:
-                        biome_counter[biome] = 0
-                    else:
-                        biome_counter[biome] += 1
+                biome = worldSlice.getBiome(addY((x, 0, z), y))
+                if biome:
+                    biome_counter[biome] += 1
+
+    majority_biome = biome_counter.most_common(1)[0][0]
     
-    majority_biome = max(biome_counter.items(), key = operator.itemgetter(1))[0]
-
-    print(f"Majority biome in the build area: {majority_biome}")
-
     return majority_biome
 
-    
+
+# Get list of blocks for each biome, builds are biome-dependent
+def get_block_list():
+    return None
+
+
+def main():
+    maj_biome = check_biome()
+    print(maj_biome)
+
+if __name__ == "__main__":
+    main()
+
