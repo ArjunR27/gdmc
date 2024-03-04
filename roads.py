54e20574e11c6006f2a4fd3bec8964609ca5a637
Kamran <kmbastani@yahoo.com>
2024-03-04 15:21:24 -0800

    Flat pathing algorithm working

--- roads.py
+++ roads.py
@@ -23,6 +23,23 @@ except InterfaceConnectionError:
     )
     sys.exit(1)
 
+try:
+    buildArea = editor.getBuildArea()
+except BuildAreaNotSetError:
+    print(
+        "Error: failed to get the build area!\n"
+        "Make sure to set the build area with the /setbuildarea command in-game.\n"
+        "For example: /setbuildarea ~0 0 ~0 ~64 200 ~64"
+    )
+    sys.exit(1)
+
+print("Loading world slice...")
+buildRect = buildArea.toRect()
+worldSlice = editor.loadWorldSlice(buildRect)
+print("World slice loaded!")
+
+heightmap = worldSlice.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
+
 
 def buildRoads(buildings):
     obstacles = []
@@ -30,21 +47,23 @@ def buildRoads(buildings):
     for building in buildings:
         print(building.corner.x - 1, building.corner.x + building.length)
         print(building.corner.z - 1, building.corner.z + building.width)
-        for x in range(building.corner.x - 1, building.corner.x + building.length):
-            for z in range(building.corner.z - 1, building.corner.z + building.width):
+        for x in range(building.corner.x - 1, building.corner.x + building.length + 1):
+            for z in range(building.corner.z - 1, building.corner.z + building.width + 1):
                 obstacles.append(Point(x, building.corner.y, z))
                 print(x, building.corner.y, z)
         print(building.door.x, building.door.y, building.door.z - 1)
         obstacles.remove(Point(building.door.x, building.door.y, building.door.z - 1))
+        obstacles.remove(Point(building.door.x, building.door.y, building.door.z))
     print(obstacles)
     """"""
     print("building roads")
-    path = astar(buildings[0], buildings[1], obstacles)
-    print("path built")
-    for block in path:
-        print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
-        editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("red_concrete"))
-        #editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("grass_block"))
+    for i in range(0, len(buildings) - 1):
+        path = astar(buildings[i], buildings[i + 1], obstacles)
+        print("path built")
+        for block in path:
+            print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
+            editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("red_concrete"))
+            #editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("grass_block"))
 
 def astar(building1, building2, obstacles):
     print("pathing")
@@ -58,7 +77,8 @@ def astar(building1, building2, obstacles):
     while not queue.empty():
         current = queue.get()
         open.remove(current.pos)
-        print("heuristic:", current.h, current.pos.x, current.pos.y, current.pos.z)
+        #editor.placeBlock((current.pos.x, current.pos.y, current.pos.z), Block("grass_block"))
+        print("heuristic:", current.h, "f:", current.f, "point:", current.pos.x, current.pos.y, current.pos.z)
         if current.h == 0:
             path = []
             current = current.parent
@@ -71,6 +91,7 @@ def astar(building1, building2, obstacles):
 
         closed.append(current.pos)
         for neighbor in current.possibleBlocks():
+            print("in obstables:", neighbor in obstacles)
             if neighbor not in obstacles and neighbor not in closed:
                 """new = Node(neighbor, current, goal)
                 if new in queue: 
@@ -80,6 +101,7 @@ def astar(building1, building2, obstacles):
                     pass
                 else:
                     print("##### new node", neighbor.x, neighbor.y, neighbor.z)
+                    print("open", len(open), "queue", queue.qsize())
                     queue.put(Node(neighbor, current, goal))
                     open.append(neighbor)
                     #time.sleep(0.5)
@@ -116,5 +138,7 @@ class Node:
         return blocks
 
 
-buildings = [Building(7, 4, 6, Point(79, 63, 16)), Building(5, 3, 6, Point(86, 63, 9))]
+buildings = [Building(7, 4, 6, Point(79, 63, 16)), Building(5, 3, 6, Point(86, 63, 9)), Building(5, 4, 6, Point(76, 63, 28))]
+#buildings = [Building(5, 3, 6, Point(86, 63, 9)), Building(5, 4, 6, Point(76, 63, 28))]
+#buildings = [Building(7, 4, 6, Point(79, 63, 16)), Building(5, 3, 6, Point(86, 63, 9))]
 buildRoads(buildings)
