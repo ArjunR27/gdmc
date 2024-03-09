36d870e85db01e05a5af5eec0f79979f22d837f0
Kamran <kmbastani@yahoo.com>
2024-03-08 22:22:31 -0800

    Update roads.py
    Cleaned up and added comments

--- roads.py
+++ roads.py
@@ -43,38 +43,33 @@ heightmap = worldSlice.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
 areaLow = buildArea.begin
 areaHigh = buildArea.end
 
-print("helooooooooo", heightmap[86 - areaLow[0]][13 - areaLow[2]])
-
 def buildRoads(buildings):
     obstacles = []
     doors = []
     goals = []
+
+    # Adding the doors to the list
     for b in buildings:
         doors.append(b.door)
 
-    print(len(buildings))
+    # Adding the buildings and 1 block padding to the obstacles list
     for building in buildings:
-        print(building.corner.x - 1, building.corner.x + building.length)
-        print(building.corner.z - 1, building.corner.z + building.width)
         for x in range(building.corner.x - 1, building.corner.x + building.length + 1):
             for z in range(building.corner.z - 1, building.corner.z + building.width + 1):
                 obstacles.append(Point(x, building.corner.y, z))
-                print(x, building.corner.y, z)
-        print(building.door.x, building.door.y, building.door.z - 1)
         obstacles.remove(Point(building.door.x, building.door.y, building.door.z - 1))
         obstacles.remove(Point(building.door.x, building.door.y, building.door.z))
-    print(obstacles)
 
+    # creating the endpoints of the highway and pathing between them
     pts = lsrl(doors)
-    #editor.placeBlock((pts[0].x, pts[0].y, pts[0].z), Block("red_concrete"))
-    #editor.placeBlock((pts[1].x, pts[1].y, pts[1].z), Block("red_concrete"))
     highway = astar(pts[0], pts[1], obstacles)
     for block in highway:
-        print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
+        #print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
         goals.append(block.pos)
         editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("red_concrete"))
         #editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("grass_block"))
 
+    # For each building path to the nearest point on the highway to path to
     for building in buildings:
         path = astar(building.door, findNearest(building.door, goals), obstacles)
         print("path built")
@@ -82,36 +77,29 @@ def buildRoads(buildings):
             print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
             editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("red_concrete"))
             #editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("grass_block"))
-    """
-    print("building roads")
-    for building in buildings:
-        path = astar(building.door, findNearest(building.door, goals), obstacles)
-        print("path built")
-        for block in path:
-            print("placing block at:", block.pos.x, block.pos.y, block.pos.z)
-            goals.append(block.pos)
-            editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("red_concrete"))
-            #editor.placeBlock((block.pos.x, block.pos.y, block.pos.z), Block("grass_block"))"""
 
+# Finds the nearest element of goals to pos
 def findNearest(pos, goals):
-    print(pos.x, pos.y, pos.z)
-    print(goals[0].x, goals[0].y, goals[0].z, "and", goals[1].x, goals[1].y, goals[1].z)
     best = None
     bestDistance = float('inf')
+
+    # Iterate through goals to find the best
     for goal in goals:
         dist = abs(pos.x - goal.x) + abs(pos.y - goal.y) + abs(pos.z - goal.z)
         if dist < bestDistance and pos != goal:
             best = goal
             bestDistance = dist
-    print("best", best.x, best.y, best.z)
     return best
 
+# Calculates a least squares regression line given the points and returns two endpoints of the line
 def lsrl(points):
+    # Calculating correlation coefficient and slope
     xValues = np.array([i.x for i in points])
     zValues = np.array([i.z for i in points])
     r = np.corrcoef(xValues, zValues)[0, 1]
     slope = r * (np.std(zValues)/np.std(xValues))
 
+    # Calculating coordinates of the endpoints of the highway
     xlow = np.min(xValues) + 5
     zlow = int(np.mean(zValues) - slope * np.mean(xValues) + slope * xlow)
     ylow = heightmap[xlow - areaLow[0]][zlow - areaLow[2]] - 1
@@ -120,8 +108,8 @@ def lsrl(points):
     yhigh = heightmap[xhigh - areaLow[0]][zhigh - areaLow[2]] - 1
     return [Point(xlow, ylow, zlow), Point(xhigh, yhigh, zhigh)]
 
+# Astar search pathing algorithm
 def astar(first, goal, obstacles):
-    print("pathing from", first.x, first.y, first.x, "to", goal.x, goal.y, goal.z)
     queue = PriorityQueue()
     start = Node(first, None, goal)
     closed = []
@@ -131,8 +119,8 @@ def astar(first, goal, obstacles):
     while not queue.empty():
         current = queue.get()
         open.remove(current.pos)
-        #editor.placeBlock((current.pos.x, current.pos.y, current.pos.z), Block("grass_block"))
-        print("heuristic:", current.h, "f:", current.f, "point:", current.pos.x, current.pos.y, current.pos.z)
+
+        # If the goal is reached, backtrack the path and return
         if current.h == 0:
             path = []
             current = current.parent
@@ -144,26 +132,21 @@ def astar(first, goal, obstacles):
             return path
 
         closed.append(current.pos)
+
+        # Adding new nodes to the queue
         for neighbor in current.possibleBlocks():
-            print("in obstables:", neighbor in obstacles)
             if neighbor not in obstacles and neighbor not in closed:
-                """new = Node(neighbor, current, goal)
-                if new in queue: 
-                    #if (new.g + new.e) < ()
-                    pass"""
                 if neighbor in open: # implement this later for better functionality
                     pass
                 else:
-                    print("##### new node", neighbor.x, neighbor.y, neighbor.z)
-                    print("open", len(open), "queue", queue.qsize())
                     queue.put(Node(neighbor, current, goal))
                     open.append(neighbor)
-                    #time.sleep(0.5)
 
     return None
 
 class Node:
     def __init__(self, pos, parent, goal):
+        # ALl of the parameters needed, including e which represents elevation
         self.pos = pos
         self.goal = goal
         self.h = abs(self.goal.x - pos.x) + abs(self.goal.z - pos.z)
@@ -171,6 +154,7 @@ class Node:
         self.e = 0
         self.parent = parent
 
+        # Double counting any change in elevation to incentivize the algorithm to avoid hills
         if parent is not None:
             self.g = parent.g + 1
             self.e = parent.e + 2 * abs(parent.pos.y - self.pos.y)
@@ -183,14 +167,9 @@ class Node:
     def __eq__(self, other):
         return self.pos == other.pos
 
+    # Children nodes
     def possibleBlocks(self):
         blocks = []
-        """blocks.append(Point(self.pos.x + 1, self.pos.y, self.pos.z))
-        blocks.append(Point(self.pos.x, self.pos.y, self.pos.z + 1))
-        blocks.append(Point(self.pos.x - 1, self.pos.y, self.pos.z))
-        blocks.append(Point(self.pos.x, self.pos.y, self.pos.z - 1))"""
-        print("hi")
-
         blocks.append(Point(self.pos.x + 1, heightmap[self.pos.x + 1 - areaLow[0]][self.pos.z - areaLow[2]] - 1, self.pos.z))
         blocks.append(Point(self.pos.x, heightmap[self.pos.x - areaLow[0]][self.pos.z + 1 - areaLow[2]] - 1, self.pos.z + 1))
         blocks.append(Point(self.pos.x - 1, heightmap[self.pos.x - 1 - areaLow[0]][self.pos.z - areaLow[2]] - 1, self.pos.z))
